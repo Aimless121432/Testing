4bd434e3e9e0e228810bb0e06d72eac3b84d2028
Aimless121432 <mb1939504@gmail.com>
2026-04-02 02:11:57 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,10 +1,10 @@
-addLayer("p", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+addLayer("T", {
+    name: "Titan", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "T", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
-		points: new Decimal(0),
+		points: new Decimal(10),
     }},
     color: "#4BDC13",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
