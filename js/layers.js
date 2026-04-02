04d3c4c50638edda8119dc1f73e098e4ddb96bf4
Aimless121432 <mb1939504@gmail.com>
2026-04-02 02:26:17 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,28 +1,35 @@
 addLayer("T", {
-    name: "Titan", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "T", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    name: "Titan",
+    symbol: "T",
+    position: 0,
     startData() { return {
         unlocked: true,
-		points: new Decimal(10),
+        points: new Decimal(0),
     }},
     color: "#4BDC13",
-    requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "titan points", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
-    baseAmount() {return player.points}, // Get the current amount of baseResource
-    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
-    gainMult() { // Calculate the multiplier for main currency from bonuses
+    requires: new Decimal(10),
+    resource: "titan points",
+    baseResource: "points",
+    baseAmount() {return player.points},
+    type: "normal",
+    exponent: 0.5,
+    gainMult() {
         mult = new Decimal(1)
         return mult
     },
-    gainExp() { // Calculate the exponent on main currency from bonuses
+    gainExp() {
         return new Decimal(1)
     },
-    row: 0, // Row the layer is in on the tree (0 is the first row)
+    row: 0,
     hotkeys: [
         {key: "t", description: "T: Reset for titan points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-    layerShown(){return true}
-})
+    layerShown(){return true},
+    upgrades: {
+        1: {
+            title: "Upgrade 1",
+            description: "Make point gain faster.",
+            cost: new Decimal(1),
+        },
+    },
+});
